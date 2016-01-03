3c959c8607a843e85f95aa782cabe2578d51cc3b
cl456852@gmail.com <cl456852@gmail.com>
2016-01-03 15:00:05 +0800

    try

--- BLL/Sis001Analysis.cs
+++ BLL/Sis001Analysis.cs
@@ -18,36 +18,39 @@ namespace BLL
         public ArrayList alys(string content, string path, string vid)
         {
             ArrayList resList = new ArrayList();
-            MatchCollection mc =idRegex.Matches( Path.GetFileNameWithoutExtension(path.ToUpper()));
-            if(mc.Count!=1)
+            try
             {
-                String unknownPath = Path.Combine(Path.GetDirectoryName(path), "sisUnknown");
-                if (!Directory.Exists(unknownPath))
-                    Directory.CreateDirectory(unknownPath);
-                File.Move(path,Path.Combine(unknownPath,Path.GetFileNameWithoutExtension(path))+".htm");
-                return resList;
-            }
-            His his = new His();
-            his.Vid = mc[0].Value.Replace("-","");
-            his.Size = Convert.ToDouble(sizeRegex.Match(path).Value.Replace("size^^^", "").Replace(".htm",""));
-            his.Html = content.Split(new string[] { "count_add_one", "下载次数:" }, StringSplitOptions.RemoveEmptyEntries)[1];
-            string torrentLink ="http://sis001.com/bbs/"+ torrentLinkRegex.Match(his.Html).Value;
-            
-            MatchCollection imgMc = imgRegex.Matches(his.Html);
-            his.Html = "";
-            foreach(Match match in imgMc)
-            {
-                if(!match.Value.Contains("torrent.gif"))
+                
+                MatchCollection mc = idRegex.Matches(Path.GetFileNameWithoutExtension(path.ToUpper()));
+                if (mc.Count != 1)
                 {
-                    his.Html += "<a href=\"" + torrentLink + "\">" + match.Value + "/></a><br>";
+                    String unknownPath = Path.Combine(Path.GetDirectoryName(path), "sisUnknown");
+                    if (!Directory.Exists(unknownPath))
+                        Directory.CreateDirectory(unknownPath);
+                    File.Move(path, Path.Combine(unknownPath, Path.GetFileNameWithoutExtension(path)) + ".htm");
+                    return resList;
                 }
-            }
+                His his = new His();
+                his.Vid = mc[0].Value.Replace("-", "");
+                his.Size = Convert.ToDouble(sizeRegex.Match(path).Value.Replace("size^^^", "").Replace(".htm", ""));
+                his.Html = content.Split(new string[] { "count_add_one", "下载次数:" }, StringSplitOptions.RemoveEmptyEntries)[1];
+                string torrentLink = "http://sis001.com/bbs/" + torrentLinkRegex.Match(his.Html).Value;
 
-                    string letter = "";
+                MatchCollection imgMc = imgRegex.Matches(his.Html);
+                his.Html = "";
+                foreach (Match match in imgMc)
+                {
+                    if (!match.Value.Contains("torrent.gif"))
+                    {
+                        his.Html += "<a href=\"" + torrentLink + "\">" + match.Value + "/></a><br>";
+                    }
+                }
+
+                string letter = "";
                 string number = "";
                 bool isEndofLetter = false;
-            foreach(char c in his.Vid)
-            {
+                foreach (char c in his.Vid)
+                {
                     if (reg1.IsMatch(c.ToString()))
                     {
                         if (isEndofLetter)
@@ -57,14 +60,23 @@ namespace BLL
                     }
                     else
                     {
-                        number +=c;
+                        number += c;
                         isEndofLetter = true;
                     }
+                }
+                his.Html += "<a href=\"https://www.google.com.tw/search?um=1&newwindow=1&safe=off&hl=zh-CN&biw=1362&bih=839&dpr=1&ie=UTF-8&tbm=isch&source=og&sa=N&tab=wi&ei=QKr6U8KMKtOWaqbigogK&q=" + his.Vid + "\"/>" + his.Vid + "</a><br>";
+                his.Html += his.Size + "<br>";
+                his.Html += "<a href=\"http://btdigg.org/search?info_hash=&q=" + letter + "+" + number + "\">" + his.Vid + "</a>\n";
+                his.Html += "<a href=\"http://www.javbus.com/" + letter + "-" + number + "\">" + his.Vid + "</a><br><br><br>\n";
+                resList.Add(his);
             }
-            his.Html += "<a href=\"https://www.google.com.tw/search?um=1&newwindow=1&safe=off&hl=zh-CN&biw=1362&bih=839&dpr=1&ie=UTF-8&tbm=isch&source=og&sa=N&tab=wi&ei=QKr6U8KMKtOWaqbigogK&q=" + his.Vid + "\"/>" + his.Vid + "</a><br>";
-            his.Html += his.Size + "<br>";
-            his.Html += "<a href=\"http://btdigg.org/search?info_hash=&q=" + letter + "+" + number + "\">" + his.Vid + "</a><br><br><br>\n";
-            resList.Add(his);
+            catch (Exception e)
+            {
+                String unknownPath = Path.Combine(Path.GetDirectoryName(path), "sisUnknown");
+                if (!Directory.Exists(unknownPath))
+                    Directory.CreateDirectory(unknownPath);
+                File.Move(path, Path.Combine(unknownPath, Path.GetFileNameWithoutExtension(path)) + ".htm");
+            }
             return resList;
         }
     }
